24bbcf9b27e18d3f134a250db061179bcc2c15e0
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-10-02 06:51:58 +0500

    add task1 and task2

---
+++ Task001/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая на входе принимает число и на выходе выдаёт его квадрат*/
+
+Console.Write("Введите число: ");
+string numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr);
+int result = number * number;
+
+Console.Write($"{number} >>> {result}");
