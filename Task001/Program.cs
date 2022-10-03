ac97b2e486bd50877d587511477785e55c70d694
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-10-03 16:03:14 +0500

    Изменили место расположения папок, разделил семинарные и домашние задачи

--- Task001/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿/*Напишите программу, которая на входе принимает число и на выходе выдаёт его квадрат*/
-
-Console.Write("Введите число: ");
-string numberStr = Console.ReadLine();
-
-int number = int.Parse(numberStr);
-int result = number * number;
-
-Console.Write($"квадратом числа {number} является {result}");
