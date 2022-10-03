d8a4a593dbe3a261adc344a06af95845feda415e
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-10-03 15:47:22 +0500

    Add task 2 for practic

---
+++ Practic tasks/Task 2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+    Console.Write($"Число {a} является большим, а число {b} меньшим");
+else if (a < b)
+    Console.Write($"Число {b} является большим, а число {a} меньшим");
